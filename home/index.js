498a484c4fa7bcbe70443874c5d37a859f74352b
xAbeer <abeerayyad2000@gmail.com>
2021-12-01 09:38:06 +0200

    edit animation navbar and name image Relates #10

--- home/index.js
+++ home/index.js
@@ -6,15 +6,6 @@ const newSlides=()=>{
     burgerIcon.addEventListener('click',()=>{
     navLinks.classList.toggle('active')
     burgerIcon.classList.toggle('fa-times')
-  //Add Animate to link
-    navLink.forEach((link,index)=>{
-        if(link.style.animation){
-            link.style.animation=''
-        }else{
-            link.style.animation=`newLink  0.5s ease forwards ${index/7+0.7}s`
-            
-        }
-    })
     })
 }
 newSlides();
