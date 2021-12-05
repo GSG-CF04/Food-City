2186d757c64e04c753dc301de60dace31b4ec9b0
hussambanna <hussambanna20@gmail.com>
2021-12-05 21:10:12 +0200

    create a pop-up thank message and make a red border for message field related to #14

--- home/index.js
+++ home/index.js
@@ -1,11 +1,27 @@
-const newSlides=()=>{
-    let burgerIcon=document.querySelector('.burgerIcon')
-    let navLinks =document.querySelector('.navLinks')
-    let navLink=document.querySelectorAll('.navLinks li ')
-//Add Toggle
-    burgerIcon.addEventListener('click',()=>{
-    navLinks.classList.toggle('active')
-    burgerIcon.classList.toggle('fa-times')
-    })
-}
+const newSlides = () => {
+  let burgerIcon = document.querySelector(".burgerIcon");
+  let navLinks = document.querySelector(".navLinks");
+  let navLink = document.querySelectorAll(".navLinks li ");
+  //Add Toggle
+  burgerIcon.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    burgerIcon.classList.toggle("fa-times");
+  });
+};
 newSlides();
+
+let textarea = document.querySelector("textarea");
+let messageThank = document.querySelector(".messageThank");
+document.querySelector(".submitBtn").addEventListener("click", function (e) {
+  if (textarea.value == "") {
+    e.preventDefault();
+    textarea.style.borderColor = "red";
+  } else {
+    e.preventDefault();
+    messageThank.style.display = "block";
+  }
+  setTimeout(function () {
+    messageThank.style.display = "none";
+  }, 1000);
+
+});
