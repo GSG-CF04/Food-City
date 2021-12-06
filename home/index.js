c049aaed838f168140627f953d21dbf5b46284b6
hussambanna <hussambanna20@gmail.com>
2021-12-06 13:26:51 +0200

    add animation to pop-up related to #14

--- home/index.js
+++ home/index.js
@@ -10,18 +10,31 @@ const newSlides = () => {
 };
 newSlides();
 
+let userName =document.querySelector (".field");
+let userEmail =document.querySelector ("#user_email");
 let textarea = document.querySelector("textarea");
+
+
 let messageThank = document.querySelector(".message_thank");
 document.querySelector(".submitBtn").addEventListener("click", function (e) {
-  if (textarea.value == "") {
+
+  if (textarea.value == "" )  {
     e.preventDefault();
     textarea.style.borderColor = "red";
   } else {
     e.preventDefault();
     messageThank.style.display = "block";
+    textarea.style.borderColor = "none";
+
   }
+
   setTimeout(function () {
+    userName.value ='';
+    userEmail.value ='';
+    textarea.value ='';
+    textarea.style.borderColor = "none";
     messageThank.style.display = "none";
+
   }, 1000);
 
 });
